b07d5c0f29822d73fab41b291331432a3b6a1649
Swapnil Thawale <92202649+Swapnil213@users.noreply.github.com>
2025-01-12 21:05:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+var elemContainer = document.querySelector("#elem-container");
+var image = document.querySelector("#fixed-image");
+
+elemContainer.addEventListener("mouseenter", function () {
+    image.style.display = "block";
 });
 
+elemContainer.addEventListener("mouseleave", function () {
+    image.style.display = "none";
+})
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+        var img = e.getAttribute("data-image");
+        image.style.backgroundImage = `url(${img})`;
+    });
+});
+
+
+
+// Swiper //
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
